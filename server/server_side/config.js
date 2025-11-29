b8ad213cd043937dd678c40e8cb076968e1d350c
patobeur <patobeur41@gmail.com>
2025-11-29 09:53:33 +0100

    new file:   public/media/characters/glb/Adventurer.glb 	new file:   public/media/characters/low/Adventurer.jpg 	new file:   public/media/structures/glb/cupolaModule.glb 	new file:   public/media/structures/glb/house.glb 	new file:   public/media/structures/glb/houseCylinder.glb 	new file:   public/media/structures/glb/spaceLanderBase.glb 	modified:   server/server_side/characters.js 	modified:   server/server_side/config.js

--- server/server_side/config.js
+++ server/server_side/config.js
@@ -6,8 +6,8 @@ const config = {
         spawnMinionsB: { x: 18, y: 18, z: 0.12, w: 2, h: .25, d: 2, type: "CylinderGeometry" },
     },
     structures: {
-        BaseTeamA: { x: -25, y: -25, z: 0, hp: 1000, type: "GLB", filepath: "./media/structures/glb/baseTeamA.glb", rotation: { x: 0, y: 180, z: 0 } },
-        BaseTeamB: { x: 25, y: 25, z: 0, hp: 1000, type: "GLB", filepath: "./media/structures/glb/baseTeamB.glb", rotation: { x: 0, y: 0, z: 0 } },
+        BaseTeamA: { x: -25, y: -25, z: 0, hp: 1000, type: "GLB", filepath: "./media/structures/glb/house.glb", rotation: { x: 0, y: 45, z: 0 }, scale: { x: 5, y: 5, z: 5 } },
+        BaseTeamB: { x: 25, y: 25, z: 0, hp: 1000, type: "GLB", filepath: "./media/structures/glb/house.glb", rotation: { x: 0, y: -135, z: 0 }, scale: { x: 5, y: 5, z: 5 } },
     },
 };
 

79a91b1ce5c66d4ce75eaee5a52724b48fd6c1b7
patobeur <patobeur41@gmail.com>
2025-11-30 15:14:43 +0100

    modified:   server/server.js

--- server/server copy.js
+++ server/server copy.js
@@ -199,24 +199,10 @@ app.post("/api/rooms/:roomId/join", requireAuth, (req, res) => {
 app.post("/api/rooms/:roomId/leave", requireAuth, (req, res) => {
 	const { roomId } = req.params;
 	try {
-		const room = roomManager.getRoom(roomId);
-		if (!room) {
-			return res.status(404).json({ error: "Room not found" });
-		}
-
 		const { removed, leftGame } = roomManager.leaveRoom(
 			roomId,
 			req.session.userId
 		);
-
-		if (removed) {
-			// Broadcast to all clients in the room that the player has left.
-			broadcastToRoom(roomId, {
-				type: "room-update",
-				players: room.getPlayersList(),
-			});
-		}
-
 		if (leftGame) {
 			broadcastToRoom(roomId, {
 				type: "player-left",
@@ -558,7 +544,7 @@ setInterval(() => {
 						id: e.id,
 						xp: e.xp,
 						maxXp: e.maxXp,
-						level: e.level,
+						level: e.level
 					});
 				}
 			});
